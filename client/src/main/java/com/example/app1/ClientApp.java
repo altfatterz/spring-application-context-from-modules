6a17bf2ffcfecf9a4c08d5a781eabd3421ed4672
Zoltan Altfatter <altfatterz@gmail.com>
2016-10-14 09:49:44 +0200

    bean autowiring

--- client/src/main/java/com/example/app1/ClientApp.java
+++ client/src/main/java/com/example/app1/ClientApp.java
@@ -1,11 +1,16 @@
 package com.example.app1;
 
 import com.example.service.ClientGreetingService;
+import com.example.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+
+import java.util.Arrays;
 
 /**
  * @author Zoltan Altfatter
@@ -17,6 +22,10 @@ public class ClientApp implements CommandLineRunner {
     @Autowired
     private ClientGreetingService clientGreetingService;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
     public static void main(String[] args) {
         SpringApplication.run(ClientApp.class, args);
     }
@@ -24,5 +33,12 @@ public class ClientApp implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         System.out.println(clientGreetingService.greet("Zoltan"));
+        System.out.println(Arrays.asList(applicationContext.getBeanDefinitionNames()));
     }
+
+    @Bean
+    GreetingService greetingService() {
+        return new HungarianGreetingService();
+    }
+
 }
--- client/src/main/java/com/example/app1/HungarianGreetingService.java
+++ client/src/main/java/com/example/app1/HungarianGreetingService.java
@@ -3,12 +3,10 @@ package com.example.app1;
 import com.example.service.GreetingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 /**
  * @author Zoltan Altfatter
  */
-@Service
 public class HungarianGreetingService implements GreetingService {
 
     private static final Logger logger = LoggerFactory.getLogger(HungarianGreetingService.class);
